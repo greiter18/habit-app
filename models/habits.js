f04c56693b4fee79ae6903e410c8e6ecfad262fa
gabriel <gabeh.reiter@gmail.com>
2022-12-13 16:55:34 -0500

    update to habits model

--- models/habits.js
+++ models/habits.js
@@ -5,10 +5,11 @@ const HabitSchema = Schema(
   {
     user_id:{ type: Number, required: true},
     title:{type: String, required: true},
-    description:{type: String, required: true},
+    description:{type: Text, required: true},
     type:{type: String, required: true},
     dates:{type: Array, required: true},
     reminder:{type: Number, required: true},
+    reminder_time:{type: Number, required: false}
     
   }, 
   {
