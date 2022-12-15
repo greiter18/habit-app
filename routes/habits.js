427065dc59ce0eab851df8d5e9ecfaf0c6f02b1c
gabriel <gabeh.reiter@gmail.com>
2022-12-15 14:04:00 -0500

    changes to habit index route

--- routes/habits.js
+++ routes/habits.js
@@ -5,18 +5,41 @@ const router = express.Router();
 router.get("/" , (_,res) => {
   const habits = [
         { _id: 1, 
-          name: "Vladimir Harkonnen", 
-          content: "Am I the drama?" },
-        {
-            _id: 2,
-            name: "Lady Jessica",
-            content: "Is Paul the Kwisatz Haderach?",
-        },
-        {
-            _id: 3,
-            name: "Paul Atreides",
-            content: "Why are my dreams so sandy?",
-        },
+          user_id: 1, 
+          title: "Start going to the gym more",
+          description: "",
+          category: "exercise",
+          reminder: true,
+          reminder_time: 250,
+          dates: [
+            {'12-11-22': {difficulty: 7, comments: "Today was not too bad"} },
+            {'12-13-22': {difficulty: 4, comments: "Felt Great"} }
+          ]
+         },
+          { _id: 2, 
+          user_id: 1, 
+          title: "Write in journal",
+          description: "",
+          category: "mental health",
+          reminder: true,
+          reminder_time: 100,
+          dates: [
+            {'10-14-22': {difficulty: 3, comments: "wrote alot today"} },
+            {'12-13-22': {difficulty: 4, comments: "Felt Great"} }
+          ]
+         },
+           { _id: 3, 
+          user_id: 2, 
+          title: "Read more books",
+          description: "",
+          category: "education",
+          reminder: false,
+          reminder_time: '',
+          dates: [
+            {'12-01-22': {difficulty: 4, comments: "Read 30 pages today"} },
+            {'12-16-22': {difficulty: 9, comments: "Barely made it"} }
+          ]
+         },
     ];
     res.json(habits);
 })
